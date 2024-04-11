378daa1d5eeaa5e7699819d8215608ff9dee3733
HitandFeyd <cchavez572@cnm.edu>
2024-04-11 16:22:22 -0600

    Add project files.

---
+++ CChavezProgramFinal/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CChavezProgramFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is my final offer
+            // Pass me. I know you can.
+            // and you have already shown a willingness to cheat on grade to cover for you
+            // and I also think you know I will know enough to keep
+            // up when I have to take .NetC# 2 with YOU in a few semeters.
+            // I guess it is all about if you really feel the need to charge me for another class.
+            // I did not have time for you to not take me seriously but i'm only taking 3 classes 
+            // this summer becasue CNM in general doesnt take it's students seriouslty. 
+   
+
+        }
+    }
+}
